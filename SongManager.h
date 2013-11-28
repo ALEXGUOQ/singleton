183097d7a0305eecd6e1438c8a810c852e26dd06
mike <275712575@qq.com>
2013-11-28 14:32:54 +0800

    单例

---
+++ SongManager.h
@@ -0,0 +1,13 @@
+//
+//  SongManager.h
+//  KoudaiTuan
+//
+//  Created by song on 13-11-28.
+//  Copyright (c) 2013年 Cao Vicky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SongManager : NSObject
++ (id)sharedManager;
+@end
---
+++ SongManager.m
@@ -0,0 +1,31 @@
+//
+//  SongManager.m
+//  KoudaiTuan
+//
+//  Created by song on 13-11-28.
+//  Copyright (c) 2013年 Cao Vicky. All rights reserved.
+//
+
+#import "SongManager.h"
+
+@implementation SongManager
++ (id)sharedManager {
+    static MyManager *sharedMyManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedMyManager = [[self alloc] init];
+    });
+    return sharedMyManager;
+}
+
+- (id)init {
+    if (self = [super init]) {
+        someProperty = [[NSString alloc] initWithString:@"Default Property Value"];
+    }
+    return self;
+}
+
+- (void)dealloc {
+    // Should never be called, but just here for clarity really.
+}
+@end
